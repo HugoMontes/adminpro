cfa2d5f2ca25a489583b0968ac9de64b81f93202
Hugo Montes <hugomontes927@gmail.com>
2018-07-29 23:08:31 -0400

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,37 +3,30 @@ import { NgModule } from '@angular/core';
 
 // Obtener la constante de rutas
 import { APP_ROUTES } from './app.routes';
-APP_ROUTES
 
+// Importar los modulos
+import { PagesModule } from './pages/pages.module';
+
+// Importar los componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+
+// Eliminar los demas imports DashboardComponent, ProgressComponent, Graficas1Component,
+// HeaderComponent, SidebarComponent, BreadcrumbsComponent, NopagefoundComponent, PagesComponent
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    NopagefoundComponent,
-    PagesComponent,
-    RegisterComponent
+    RegisterComponent,
+    // Eliminar los: DashboardComponent, ProgressComponent, Graficas1Component,
+    // HeaderComponent, SidebarComponent, BreadcrumbsComponent, NopagefoundComponent, PagesComponent    
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule, // Importar el modulo PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
